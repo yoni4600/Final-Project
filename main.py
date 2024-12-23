d7e17276bec659b65651e0c987921302eee09353
yoni4600 <yoni4600@gmail.com>
2024-12-23 22:46:51 +0200

    organize files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import networkx as nx
 from netorch.dataset import load_edgelist, load_labels
-from evaluation_plan import EvaluationPlan
+from src.evaluation_plan import EvaluationPlan
 
 
 def main():
